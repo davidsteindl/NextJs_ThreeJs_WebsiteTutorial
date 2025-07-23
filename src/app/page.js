f0bf8ef9f9b1ac3ac854e26bfd000a50d0582639
David Steindl <davidsteindl0@gmail.com>
2025-07-23 16:07:03 +0200

    Added Custom Images as Backgrounds

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import bg from '../../public/background/HomePage_Background.png'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-   
+    <main className="flex min-h-screen flex-col items-center justify-between relative">
+  <Image className="w-full h-full object-cover object-center opacity-25" src={bg} alt="background-image" /> 
     </main>
   );
 }
