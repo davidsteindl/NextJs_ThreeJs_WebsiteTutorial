e0c93283c3d53006fdb7acf9f62e0283a7758040
David Steindl <davidsteindl0@gmail.com>
2025-07-31 18:12:00 +0200

    Navigation-Buttons: Added the circular positioning of navigation buttons in index.jsx

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import bg from '../../public/background/HomePage_Background.png'
 import RenderModel from "@/components/RenderModel";
 import ElfWizard from "@/components/models/elfWizard";
+import Navigation from "@/components/navigation";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <Image className="w-full h-full object-cover object-center opacity-25" src={bg} alt="background-image" fill />
 
       <div className="w-full h-screen">
+        <Navigation/>
         <RenderModel>
           <ElfWizard />
         </RenderModel>
