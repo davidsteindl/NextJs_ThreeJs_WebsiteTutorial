da55c93da2f30f25a8e6989f5baaade79e836ab7
David Steindl <davidsteindl0@gmail.com>
2025-08-01 11:25:55 +0200

    Navigation-Buttons: Added labels for the navigation buttons

--- src/components/navigation/NavButton.jsx
+++ src/components/navigation/NavButton.jsx
@@ -25,7 +25,7 @@ const getIcon = (icon) => {
         case "linkedin":
             return <Linkedin className='w-full h-auto' strokeWidth={1.5} />
 
-            case "instagram":
+        case "instagram":
             return <LucideInstagram className='w-full h-auto' strokeWidth={1.5} />
 
         case "resume":
@@ -41,10 +41,16 @@ const NavButton = ({ x, y, label, link, icon, newTab }) => {
     return (
         <div className='absolute cursor-pointer z-50'
             style={{ transform: `translate(${x}, ${y})` }}>
-            <Link href={link} target={newTab ? '_blank' : '_self'} className='group text-foreground rounded-full flex items-center justify-center
+            <Link href={link} target={newTab ? '_blank' : '_self'} className='text-foreground rounded-full flex items-center justify-center
             bg-red-900/20 border-accent/20 border-solid backdrop-blur-[6px] shadow-glass-inset hover:shadow-glass-sm' aria-label={label} name={label}>
                 <span className='relative w-14 h-14 p-4 animate-spin-slow-reverse group-hover:pause group-hover:text-accent'>
                     {getIcon(icon)}
+
+                    <span className='peer bg-transparent absolute top-0 left-0 w-full h-full' />
+                    <span className='absolute hidden peer-hover:block px-2 py-1 left-full mx-2 top-1/2 -transalte-y-1/2
+                bg-background text-foreground text-sm rounded-md shadow-lg whitespace-nowrap'>
+                        {label}
+                    </span>
                 </span>
             </Link>
         </div>
