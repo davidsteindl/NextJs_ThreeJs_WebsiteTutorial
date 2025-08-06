856903530b4f7ccf5229f7791f4da08f3eb10210
David Steindl <davidsteindl0@gmail.com>
2025-08-07 00:52:34 +0200

    Projects-Page: Added a scythe 3D-model to the projects page

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -2,6 +2,8 @@ import Image from "next/image";
 import bg from '../../../public/background/ProjectsPage_Background.png'
 import ProjectList from "@/components/projects";
 import { projectsData } from "../data";
+import RenderModel from "@/components/RenderModel";
+import WizardScythe from "@/components/models/wizardScythe";
 
 
 export default function Home() {
@@ -10,6 +12,12 @@ export default function Home() {
       <Image className="w-full h-full object-cover object-center opacity-25 -z-20" src={bg} alt="background-image" fill />
 
     <ProjectList projects= {projectsData} />
+
+   <div className='flex items-center justify-center fixed top-20 left-0 h-screen'>
+      <RenderModel>
+          <WizardScythe />
+        </RenderModel>
+   </div>
     </main>
   );
 }
