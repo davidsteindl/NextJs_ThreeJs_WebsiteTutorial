82e9ad6d6b5f3cccdfb3e40b96f40ab7cf3f352e
David Steindl <davidsteindl0@gmail.com>
2025-08-07 00:15:11 +0200

    Fireflies-Background: Fixed errors

--- src/components/FireFliesBackground.jsx
+++ src/components/FireFliesBackground.jsx
@@ -1,39 +1,41 @@
-import React from 'react'
+"use client"
+import React, { useEffect, useState } from 'react'
 
 const createFireFly = () => ({
     id: Math.random(),
-    top: `$Math.random()*100}%`,
-    left: `$Math.random()*100}%`,
+    top: `${Math.random() * 100}%`,
+    left: `${Math.random() * 100}%`,
     animationDuration: `${Math.random() * 5 + 5}s`
 })
-
+;
 const FireFliesBackground = () => {
 
-    const [fireFlies, setfireFlies] = useState([])
+    const [fireFlies, setfireFlies] = useState([]);
+
     useEffect(() => {
 
         const addFireFlyPeriodically = () => {
             const newFirefly = createFireFly();
-            setfireFlies([currentFireflires => [
-                ...currentFireflies.slice(-14), 
-                newFireFly
-            ]]);
+            setfireFlies(currentFireflies => [
+                ...currentFireflies.slice(-14),
+                newFirefly
+            ]);
         };
 
         const interval = setInterval(addFireFlyPeriodically, 1000);
         return () => clearInterval(interval);
 
-    }, [])
+    }, []);
 
 
     return (
         <div className='fixed top-0 left-0 w-full h-full -z-10 overflow-hidden'>
-            {fireflies.map((firefly) => {
+            {fireFlies.map((firefly) => {
                 return <div key={firefly.id} classname='absolute rounded-full bg-accent w-[10px] h-[10px] bg-firefly-radial'
                     style={{
                         top: firefly.top,
                         left: firefly.left,
-                        animation: `move ${firefly.animationDuration} infinite alternate)`
+                        animation: `move ${firefly.animationDuration} infinite alternate`
                     }}></div>
             })}
         </div>
