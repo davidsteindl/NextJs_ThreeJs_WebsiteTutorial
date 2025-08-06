fc0e11a71e01b0449bac6988b2081a183ec83e46
David Steindl <davidsteindl0@gmail.com>
2025-08-07 00:16:13 +0200

    Projects-Page: Added a projects page and displayed projects data

---
+++ src/app/projects/page.js
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import bg from '../../../public/background/ProjectsPage_Background.png'
+import ProjectList from "@/components/projects";
+import { projectsData } from "../data";
+
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between relative">
+      <Image className="w-full h-full object-cover object-center opacity-25 -z-20" src={bg} alt="background-image" fill />
+
+    <ProjectList projects= {projectsData} />
+    </main>
+  );
+}
