49679755ab835aa1592f5f7fdb88e948ecd9b8c0
David Steindl <davidsteindl0@gmail.com>
2025-08-12 17:43:31 +0200

    Resources: Changed depths of layout on main and projects page

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,7 @@ import Navigation from "@/components/navigation";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between relative">
-      <Image className="w-full h-full object-cover object-center opacity-25 -z-20" src={bg} alt="background-image" fill />
+      <Image className="w-full h-full object-cover object-center opacity-25 -z-50" src={bg} alt="background-image" fill />
 
       <div className="w-full h-screen">
         <Navigation/>
--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -9,7 +9,7 @@ import WizardScythe from "@/components/models/wizardScythe";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between relative">
-      <Image className="w-full h-full object-cover object-center opacity-25 -z-20" src={bg} alt="background-image" fill />
+      <Image className="-z-50 fixed top-0 left-0 w-full h-full object-cover object-center opacity-50" src={bg} alt="background-image" fill />
 
     <ProjectList projects= {projectsData} />
 
