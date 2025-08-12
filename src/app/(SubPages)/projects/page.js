0c2290a559a03257f00e9450fa66bd16376a6ef5
David Steindl <davidsteindl0@gmail.com>
2025-08-12 21:00:00 +0200

    SubPages: Added a fixed layout for the sub pages

--- src/app/(SubPages)/projects/page.js
+++ src/app/(SubPages)/projects/page.js
@@ -1,15 +1,15 @@
 import Image from "next/image";
-import bg from '../../../public/background/ProjectsPage_Background.png'
+import bg from '../../../../public/background/ProjectsPage_Background.png'
 import ProjectList from "@/components/projects";
-import { projectsData } from "../data";
+import { projectsData } from "../../data";
 import RenderModel from "@/components/RenderModel";
 import WizardScythe from "@/components/models/wizardScythe";
 
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between relative">
-      <Image className="-z-50 fixed top-0 left-0 w-full h-full object-cover object-center opacity-50" src={bg} alt="background-image" fill />
+    <>
+      <Image className="-z-50 fixed top-0 left-0 w-full h-full object-cover object-center opacity-50" src={bg} alt="background-image"  />
 
     <ProjectList projects= {projectsData} />
 
@@ -18,6 +18,6 @@ export default function Home() {
           <WizardScythe />
         </RenderModel>
    </div>
-    </main>
+    </>
   );
 }
