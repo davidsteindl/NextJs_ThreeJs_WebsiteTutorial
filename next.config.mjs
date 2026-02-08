7804fde77c9990d8e73f3dd2fd373fb6b3048ca7
David Steindl <101907077+davidsteindl@users.noreply.github.com>
2026-02-09 00:24:20 +0100

    Updated next.config.mjs for Gihtub Pages deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: 'NextJs_ThreeJs_WebsiteTutorial',
+  assetPrefix: 'NextJs_ThreeJs_WebsiteTutorial',
+}
 
+module.exports = nextConfig
 export default nextConfig;
